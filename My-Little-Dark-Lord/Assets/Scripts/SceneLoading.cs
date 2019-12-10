d76e273b6c507b141e0d13c32b207e16e8eb3ea3
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-12-09 17:13:32 -0700

    Loading Screen

---
+++ My-Little-Dark-Lord/Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image _progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //create async op
+        
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+        //take progress bar fill = async operation progress.
+        while (gameLevel.progress < 1)
+        {
+            print("Progressing: " + gameLevel.progress);
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+        //when finished load the game scene
+    }
+}
