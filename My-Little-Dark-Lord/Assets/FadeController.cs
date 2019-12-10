8af311a9c33d4d8fc892139c38eeb94a23ef0de3
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-12-09 20:38:04 -0700

    Progress

---
+++ My-Little-Dark-Lord/Assets/FadeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    public Animator animator => GetComponent<Animator>();
+
+    public void FadeIn()
+    {
+        animator.SetBool("Fade", true);
+    }
+    public void FadeOut()
+    {
+        animator.SetBool("Fade", false);
+    }
+}
